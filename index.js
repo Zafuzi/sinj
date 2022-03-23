50a1d19e33c03a58002805cefcbddaac4a0be8bd
Zafuzi <zacharyfoutz@gmail.com>
2022-03-23 10:13:30 -0700

    * added basic server side routing

--- index.js
+++ index.js
@@ -20,41 +20,94 @@ const sleepless         = require("sleepless");
 const HERE  = path.dirname(module.filename);
 const app   = connect();
 
-let layoutTemplate = fs.readFileSync("/static/layout.html");
+let layoutTemplate = fs.readFileSync(HERE + "/static/layout.html").toString();
 
 const routes = {
-    home: {
-        content: "/static/pages/home/home.html",
-        stylesheets: "/static/pages/home/home.css",
-        scripts: "/static/pages/home/home.js",
+    "": { // means default route i.e https://site.com
+        // stylesheets, scripts, and content assume they are being served from /static/
+        content: "pages/home/home.html",
+        stylesheets: ["pages/home/home.css"], 
+        scripts: ["pages/home/home.js"],
+        title: "Home",
         before: function(okay, fail)
         {
-            okay(data);
         },
         during: function(data)
         {
-           rplc8(contentTemplate, "#posts", "/posts", data);
+           //rplc8(contentTemplate, "#posts", "/posts", data);
         },
         after: function(data)
         {
-
         }
     }
 }
 
 app.use(function(request, response, next)
 {
-    let route = request.params;
+    let {_parsedUrl} = request;
+    let requestedRoute = _parsedUrl.pathname.split("/");
+        requestedRoute = requestedRoute[requestedRoute.length - 1];
 
-    route.before(function(data)
+
+    //console.log(_parsedUrl, requestedRoute)
+
+    let route = routes[requestedRoute];
+
+    // console.log(route);
+
+    let stylesheetString = "";
+    let contentString = "";
+    let scriptsString = "";
+    let titleString = (route && route.title) || "Nodes - " + requestedRoute;
+
+    if(route && route.content && route.stylesheets && route.scripts)
     {
-    }, fail);
-    //response.send();
-    next();
+        try
+        {
+            contentString = fs.readFileSync(HERE + "/static/" + route.content).toString();
+            if(contentString)
+            {
+                // rplc8
+                //console.log(contentTemplate);
+            }
+        }
+        catch(error)
+        {
+            console.error("FAILED to read file: %s/static/%s", HERE, route.content);
+        }
+
+        for(let stylesheet of route.stylesheets)
+        {
+            stylesheetString += `<link rel="stylesheet" href="${stylesheet}">`;
+        }
+
+        for(let script of route.scripts)
+        {
+            scriptsString += `<script src="${script}"></script>`;
+        }
+    }
+
+    layoutTemplate = layoutTemplate.replace(/{{stylesheets}}/gm, stylesheetString);
+    layoutTemplate = layoutTemplate.replace(/{{content}}/gm, contentString);
+    layoutTemplate = layoutTemplate.replace(/{{scripts}}/gm, scriptsString);
+    layoutTemplate = layoutTemplate.replace(/{{title}}/gm, titleString);
+
+    //console.log(layoutTemplate)
+
+    if(route)
+    {
+        response.end(layoutTemplate);
+    }
+    else
+    {
+        next();
+    }
 })
 
-console.log(HERE)
+// console.log(HERE)
 
 app.use(require("serve-static")(HERE + "/static"));
+
+// TODO need to add case for 404 not found
 
 module.exports = app;
--- static/pages/home/home.js
+++ static/pages/home/home.js
@@ -1,6 +1,9 @@
-posts = [
+const posts = [
     {id: 0},
     {id: 1},
     {id: 2},
     {id: 3},
 ];
+
+
+console.log(posts);
