2b2a15aa6521460b63c5b5ecfaaf5b2ba01bc283
Zafuzi <zacharyfoutz@gmail.com>
2022-08-26 21:59:35 -0700

    * removed uneeded imports on server

--- index.js
+++ index.js
@@ -12,13 +12,9 @@
 delete require.cache[module.filename];	// always reload
 
 /* REQUIRES */
-const path              = require("path");
-const connect           = require("connect");
-const rpc               = require("rpc");
-const serve             = require("serve-static");
-const fs                = require("fs");
-const sleepless         = require("sleepless");
-const cors				= require("cors");
+const path = require("path");
+const rpc = require("rpc");
+const serve = require("serve-static");
 
 const HERE  = path.dirname(module.filename);
 let app = require( "rpc" )( "/api/", HERE + "/api/", { cors: true, dev: true } );
@@ -33,14 +29,5 @@ app.use((req, res, next) =>
 	res.setHeader("X-WebKit-CSP", "default-src 'self'");
 	next();
 });
-
-console.log(process.argv);
-if(process.argv.indexOf("dev") !== -1) 
-{
-	const server = app.listen(0, function()
-	{
-		console.log("listening to everything on: http://127.0.0.1:" + server.address().port);
-	});
-}
 
 module.exports = app;
