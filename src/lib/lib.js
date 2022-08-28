a28a50655da20f8c8110320ada088a5ddffeec38
Zafuzi <zacharyfoutz@gmail.com>
2022-08-28 13:35:00 -0700

    * updated listen to allow elements or array of elements/strings

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -316,6 +316,46 @@ export const listen = function(selector, eventType, onEventFunction)
 		return false;
 	}
 
+	// handle single HTMLElement
+	if(selector instanceof HTMLElement)
+	{
+		selector.addEventListener(eventType, onEventFunction);
+		return true;
+	}
+
+	// Handle multiple HTMLElements in an array
+	if(selector instanceof Array)
+	{
+		selector.forEach(s =>
+		{
+			if(s instanceof HTMLElement)
+			{
+				s.addEventListener(eventType, onEventFunction);
+			}
+
+			if(s instanceof String)
+			{
+				let elements = document.querySelectorAll(s);
+				if(!elements)
+				{
+					console.error("Element not found, add listener failed...", s);
+					return false;
+				}
+
+				if(elements.length > 0)
+				{
+					for(let i = 0; i < elements.length; i++)
+					{
+						let e = elements[i];
+						e.addEventListener(eventType, onEventFunction);
+					}
+				}
+			}
+		});
+
+		return true;
+	}
+
 	let elements = document.querySelectorAll(selector);
 	if(!elements)
 	{
@@ -338,3 +378,18 @@ export const listen = function(selector, eventType, onEventFunction)
 	return true;
 };
 
+export const getQueryData = function()
+{
+	let o = {};
+	let s = document.location.search;
+	if(s)
+	{
+		let kv = s.substring(1).split("&");
+		for(let i = 0; i < kv.length; i++)
+		{
+			let aa = kv[i].split("=");
+			o[aa[0]] = decodeURIComponent(aa[1]);
+		}
+	}
+	return o;
+};
