0c05756e567bc782b957cb48fdb1f6303e12077a
Zachary Foutz <zachary@boomlearning.com>
2023-03-24 10:40:05 -0700

    * updated npm packages * always cleared apis.js

--- api/apis.js
+++ api/apis.js
@@ -1,3 +1,6 @@
+// always clear from cache
+delete require.cache[module.filename];
+
 /*
 	We destructure the module.exports of each module we want to include as an API
 	so the final structure looks like
