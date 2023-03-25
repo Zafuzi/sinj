c37efcb3c68d77f52df29d6c4f116c398f56ed66
Joe Hitchens <joe@sleepless.com>
2023-03-25 00:52:49 +0000

    added some debug lines to show data going into and out of the rpc module

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ delete require.cache[module.filename];	// always reload
 const path = require("path");
 const HERE = path.resolve(__dirname);
 const sleepless = require("sleepless");
-const L = sleepless.L.mkLog("--- server\t\t")(5);
+const L = sleepless.L.mkLog("\t")(5);
 
 const { o2j, } = sleepless;
 
@@ -18,9 +18,18 @@ const methods = {
     }
 }
 
-module.exports = function(input, okay, fail)
+module.exports = function(input, _okay, _fail)
 {
-    L.D( "input: "+o2j(input) );
+    L.D( "C ---> S "+o2j(input) );
+
+    const okay = function( data ) {
+        L.D( "C <=== S "+o2j(input) );
+        _okay( data );
+    };
+    const fail = function( error ) {
+        L.D( "C <*** S "+o2j(input) );
+        _fail( error );
+    };
 
 	const {action} = input;
 
@@ -33,6 +42,10 @@ module.exports = function(input, okay, fail)
         okay(methods[action](input));
 	}
 	catch(e) {
+        // This is only going to catch exceptions if every method function (like ping)
+        // is fully synchonous, which seems unlikely.
+        // The only way for it to work would be force everything in here to use promises, 
+        // catch all the exceptions and funnel them through here.
         L.E(e.message);
 		fail({ message: e.message, action});
 	}
