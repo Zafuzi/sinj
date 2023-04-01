b4978e710e9aa1af467adf8176e23459be09613c
Zachary Foutz <zachary@boomlearning.com>
2023-04-01 16:45:25 -0700

    * made navAnchors switch active link faster for illusion * added Server helper

--- client/layout.js
+++ client/layout.js
@@ -1,1 +1,43 @@
 console.log("layout.js loaded");
+
+// select nav - anchors
+const navAnchors = document.querySelectorAll("nav a");
+
+for(let anchor of navAnchors)
+{
+    anchor.addEventListener("click", (event) =>
+    {
+        navAnchors.forEach(anchor => anchor.classList.remove("active"));
+        event.target.classList.add("active");
+    });
+}
+
+Server = {
+    async get(url)
+    {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+        
+        return response.json();
+    },
+    async post(action, data)
+    {
+        data.action = action;
+        
+        const response = await fetch("/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        
+        return response.json();
+    }
+}
