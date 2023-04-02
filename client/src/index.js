ccfd9f6e01798d3ca5f29aa2f593b6c47d3e01c7
Zachary Foutz <zachary@boomlearning.com>
2023-04-02 14:08:53 -0700

    * moved all of app.js to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,26 @@ import "coughdrop/general.import.less";
 import "coughdrop/fonts.import.less";
 import "./water.min.css";
 import './app.less';
-import App from './app';
+
+import {Router} from 'preact-router';
+import {getUserData} from "./lib";
+
+import Header from './components/header/header.js';
+import Home from './routes/home.js';
+import About from './routes/about.js';
+import Login from './routes/login.js';
+
+const App = () => (
+    <div id="app">
+        <Header />
+        <main className={"container-1100 container-centered"}>
+            <Router>
+                <Home path="/" />
+                <About path="/about" />
+                <Login path="/login" />
+            </Router>
+        </main>
+    </div>
+);
 
 export default App;
