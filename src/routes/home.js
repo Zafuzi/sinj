7a8ead825a4cba665fc513230964ae2e34025043
Zachary Foutz <zachary@boomlearning.com>
2023-04-02 09:58:12 -0700

    * updated home to track loggedIn state

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,11 @@
+import {sid, isLoggedIn} from "../lib";
+
 export default function()
 {
     return (
-        <h1>Welcome home sailor...</h1>
+        <section className={"container-1100 container-centered container-padded"}>
+            {isLoggedIn() && <h1>Welcome home friend... {sid.value}</h1>}
+            {!isLoggedIn() && <h1>Welcome home stranger... {sid.value}</h1>}
+        </section>
     );
 };
