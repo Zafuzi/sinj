6ee72d1b02a9e432ff58f2ede8b8b013f0b39019
Zachary Foutz <zachary@boomlearning.com>
2023-04-01 21:37:57 -0700

    * moved to using preact

--- client/sleepless.js
+++
@@ -1,1 +0,0 @@
-../node_modules/sleepless/sleepless.js
--- index.js
+++ index.js
@@ -3,18 +3,10 @@ delete require.cache[module.filename];	// always reload
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.NODE_ENV || "production";
 
-const isProd = ENV === "production";
 const isDev = ENV === "development";
 
-const clientPrefix = isProd ? "dist/client" : "client";
-const publicPrefix = isProd ? "dist/public" : "public";
-const serverPrefix = isProd ? "dist/server" : "server";
-
-const express = require("express");
 const path = require("path");
-const routes = require("./routes");
-const url = require('url');
-const expressHandlebars = require("express-handlebars");
+const express = require("express");
 
 const settings = require("./settings");
 
@@ -30,63 +22,24 @@ app.use((req, res, next) =>
     next();
 });
 
-app.use(express.static(path.resolve(__dirname, clientPrefix)));
-app.use(express.static(path.resolve(__dirname, publicPrefix)));
-
-app.engine("handlebars", expressHandlebars.engine());
-app.set("views", path.resolve(__dirname, clientPrefix));
-app.set("view engine", "handlebars");
-
-app.get("*", async(req, res) =>
-{
-    const baseRoute = req.url.split("?")[0];
-    const route = routes.find(r => r.path === baseRoute);
-    const queryData = new URLSearchParams(url.parse(req.url).query);
-    const renderData = {
-        layout: "layout",
-        helpers: require(path.resolve(__dirname, clientPrefix, "helpers")),
-        baseRoute
-    };
-    
-    if(!route)
-    {
-        return res.render("pages/404", renderData);
-    }
-    
-    if(route.layout)
-    {
-        renderData.layout = route.layout;
-    }
-    
-    if(route.onBeforeAction instanceof Function)
-    {
-        renderData.data = await route.onBeforeAction(queryData);
-    }
-    
-    renderData.route = {
-        path: route.path,
-        name: route.name
-    }
-    
-    res.render(route.view, renderData);
-});
+app.use(express.static(path.resolve(__dirname, "build")));
 
 app.post("*", bodyParser, (req, res) =>
 {
-    const methods = require(path.resolve(__dirname, serverPrefix, "methods"));
+    const methods = require(path.resolve(__dirname, "server", "methods"));
     const body = req.body;
     const action = body?.action;
-    
+
     L.V(`C >>>> S | method: ${req.method} | action: ${action}`);
-    
+
     const _okay = function( data ) {
         return res.json({error: null, result: data});
     }
-    
+
     const _fail = function( error ) {
-        return res.json({error: error, result: null});
+        return res.json({error, result: null});
     }
-    
+
     if(!action)
     {
         _fail("No action specified");
@@ -94,11 +47,17 @@ app.post("*", bodyParser, (req, res) =>
 
     if(!methods[action])
     {
-        return _fail("Method not found: " + action);
+        return _fail(`Method not found: ${  action}`);
     }
-    
+
     methods[action](req.body, _okay, _fail);
 });
+
+app.get("*", (req, res) =>
+{
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
 
 if(isDev)
 {
