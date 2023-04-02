bd861209605570b79ccd6b366c8d3ff7c7b03984
Zachary Foutz <zachary@boomlearning.com>
2023-04-02 14:09:04 -0700

    * updated home.js to show userData

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,11 +1,39 @@
-import {sid, isLoggedIn, Session} from "../lib";
+import {getUserData, isLoggedIn, Session} from "../lib";
+import {Component} from "preact";
 
-export default function()
-{
-    return (
-        <section className={"container-1100 container-centered container-padded"}>
-            {isLoggedIn() && <h1>Welcome home friend... {Session.get("sid")}</h1>}
-            {!isLoggedIn() && <h1>Welcome home stranger...</h1>}
-        </section>
-    );
+class WelcomeSection extends Component {
+    render (props, {}, context)
+    {
+        return (
+            <div>
+                <h1>Welcome home {props.user.username}</h1>
+                <p>Your session id: {Session.get("sid")}</p>
+            </div>
+        )
+    }
+}
+
+export default class Home extends Component {
+    state = {
+        user: {
+            username: ""
+        }
+    }
+    
+    async componentDidMount()
+    {
+        const instance = this;
+        const data = await getUserData();
+        instance.setState({user: data?.user});
+    }
+
+    render (props, {user}, context)
+    {
+        return (
+            <section className={"container-1100 container-centered container-padded"}>
+                {!isLoggedIn() && <h1>Welcome home stranger...</h1>}
+                {isLoggedIn() && <WelcomeSection user={user} />}
+            </section>
+        )
+    }
 };
