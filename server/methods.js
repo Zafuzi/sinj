6fa0c8566463515603e4853478743ee60bf92e8e
Zachary Foutz <zachary@boomlearning.com>
2023-04-02 09:57:09 -0700

    * made register and login do something on the server

--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,7 @@
+delete require.cache[module.filename];	// always reload
+
+const users = require("sleepless-users");
+
 module.exports = {
     ping(input, okay)
     {
@@ -5,14 +9,30 @@ module.exports = {
     },
     login(input, okay, fail)
     {
-        const {email, password} = input;
-        
-        fail("Not implemented");
+        const {username, email, password} = input;
+        users.connect("sqlite3", {}, (db) =>
+        {
+            db.authenticate({username, email, password}, (result) =>
+            {
+                okay(result);
+            }, fail);
+        }, fail);
     },
     register(input, okay, fail)
     {
-        const {email, password} = input;
-        
-        fail("Not implemented");
+        const {username, email, password} = input;
+        users.connect("sqlite3", {}, (db) =>
+        {
+            db.register({username, email, password}, (result) =>
+            {
+                db.authenticate({username, email, password}, (result) =>
+                {
+                    okay(result);
+                }, function(error)
+                {
+                    fail("User was created, but could not be authenticated. Please try logging in again.", error);
+                });
+            }, fail);
+        }, fail);
     }
-}
+};
